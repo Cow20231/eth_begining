1da8f4f89c1a4f95e06547cd288d1ace4e9a3f70
Cow20231 <142592160+Cow20231@users.noreply.github.com>
2023-08-18 17:08:42 +0530

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -5,16 +5,19 @@ string public name ;
 string public symbol ;
 mapping (address => uint256) public balance ;
 uint256 public total_Supply ;
+address  owner ;
  constructor (string memory _name , string memory _symbol  , uint256 initial_supply)  {
      name = _name ;
      symbol = _symbol ;
      balance[msg.sender] += initial_supply ;
      total_Supply += initial_supply ;
-
+     owner =  msg.sender ; 
 }
+
     function mint(address _address , uint256 _amount) public {
-        balance[_address] += _amount ;
-        total_Supply += _amount ;
+        require(msg.sender == owner ) ; //  mint only when onwer is calling the mint fucntion 
+        balance[_address] += _amount ; // balance is added to the mapping address to amount balance 
+        total_Supply += _amount ; 
     }
 
     function burn (address _address , uint256 _amount) public   {
@@ -22,4 +25,5 @@ uint256 public total_Supply ;
         balance[_address] -= _amount ;
         total_Supply -= _amount ;
     }
+
 }
